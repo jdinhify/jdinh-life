97445d74428f8f4ef5ae6506c8b2ea72175467ae
JD <me@jdinh.life>
2016-09-27 14:08:19 +1000

    more config, changed Header to not extends Component

--- web_modules/Header/index.js
+++ web_modules/Header/index.js
@@ -1,24 +1,18 @@
-import React, { Component, PropTypes } from 'react'
+import React from 'react'
 
 import styles from './index.scss'
 
-export default class Header extends Component {
-
-  static contextTypes = {
-    metadata: PropTypes.object.isRequired
-  };
-
-  render() {
-
-    return (
-      <header className={styles.header}>
-        <div className={styles.logo}>
-          <a className={styles.logoLink} href="/">{'JD\'s'}</a>
-        </div>
-        <nav className={styles.nav}>
-          <button className={styles.navButton} >{'Button'}</button>
-        </nav>
-      </header>
-    )
-  }
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <a className={styles.logoLink} href="/">{'JD\'s'}</a>
+      </div>
+      <nav className={styles.nav}>
+        <button className={styles.navButton} >{'Button'}</button>
+      </nav>
+    </header>
+  )
 }
+
+export default Header
