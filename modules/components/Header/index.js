0fa490ec1f5f1c7ba45b0eed5f0fbd86b1a00674
JD <me@jdinh.life>
2016-10-30 12:53:01 +1100

    removed sticky header

--- modules/components/Header/index.js
+++ modules/components/Header/index.js
@@ -5,49 +5,12 @@ import styles from './index.scss'
 
 export default class Header extends Component {
 
-  componentWillMount() {
-    this.setState({
-      scrolled: false
-    })
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll)
-  }
-
-  handleScroll = (event) => {
-    const scrollTop = event.srcElement.body.scrollTop
-
-    if (!this.state.scrolled && scrollTop > 16) {
-      this.setState({
-        ...this.state,
-        scrolled: true
-      })
-    } else if (this.state.scrolled && scrollTop <= 16) {
-      this.setState({
-        ...this.state,
-        scrolled: false
-      })
-    }
-  }
-
-  headerClassName() {
-    return this.state && this.state.scrolled ? styles.headerScrolled : styles.header
-  }
-
   render() {
     return (
-      <header className={this.headerClassName()}>
+      <header className={styles.header}>
         <div className={styles.logo}>
           <Link className={styles.logoLink} to="/">{'JD\'s'}</Link>
         </div>
-        <nav className={styles.nav}>
-          <button className={styles.navButton} >{'Button'}</button>
-        </nav>
       </header>
     )
   }
