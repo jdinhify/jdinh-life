ec6c16210225ce97280b33711f104fef0cd2eaf2
JD <me@jdinh.life>
2016-10-30 17:09:09 +1100

    introduced 'hide' property

--- modules/containers/Homepage/index.js
+++ modules/containers/Homepage/index.js
@@ -4,8 +4,6 @@ import enhanceCollection from 'phenomic/lib/enhance-collection'
 import { Page } from 'containers'
 import { PagesList } from 'components'
 
-const numberOfLatestPosts = 6
-
 export default class Homepage extends Component {
   static contextTypes = {
     collection: PropTypes.array.isRequired
@@ -13,11 +11,10 @@ export default class Homepage extends Component {
 
   render() {
     const latestPosts = enhanceCollection(this.context.collection, {
-      filter: { layout: 'Post' },
+      filter: ({ layout, hide }) => (layout === 'Post') && (!hide),
       sort: 'date',
       reverse: true
     })
-    .slice(0, numberOfLatestPosts)
 
     return (
       <Page {...this.props} displayTitle={false}>
