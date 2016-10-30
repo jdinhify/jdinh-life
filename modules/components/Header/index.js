a6276a0c0a097a14e8c268ff70beaf69af275a8c
JD <me@jdinh.life>
2016-10-30 16:39:35 +1100

    added menu to Header

--- modules/components/Header/index.js
+++ modules/components/Header/index.js
@@ -4,12 +4,41 @@ import { Link } from 'react-router'
 import styles from './index.scss'
 
 export default class Header extends Component {
+  componentWillMount() {
+    this.setState({
+      navActive: false
+    })
+  }
+
+  toggleNavClass() {
+    this.setState({
+      ...this.state,
+      navActive: !this.state.navActive
+    })
+  }
+
+  handleNavToggle = () => {
+    this.toggleNavClass()
+  }
+
+  renderNavClass() {
+    return `${styles.nav} ${this.state.navActive ? styles.navActive : styles.navInactive}`
+  }
 
   render() {
     return (
       <header className={styles.header}>
-        <div className={styles.logo}>
-          <Link className={styles.logoLink} to="/">{'JD\'s'}</Link>
+        <div className={styles.wrapper}>
+          <div className={styles.logo}>
+            <Link className={styles.logoLink} to="/">{'JD\'s'}</Link>
+          </div>
+          <div
+            className={styles.navToggle}
+            onClick={this.handleNavToggle}
+            >{'Nav Toggle'}</div>
+          <div className={this.renderNavClass()}>
+            <Link to="/" className={styles.navItem}>{'Home'}</Link>
+          </div>
         </div>
       </header>
     )
